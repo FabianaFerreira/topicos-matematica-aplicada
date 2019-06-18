d4ed2518c8489336b78ee6af22d61dc066f554ec
Fabiana Ferreira Fonseca <fabiana.ferreira@poli.ufrj.br>
2019-06-18 16:23:44 -0300

    arquivo python

---
+++ Teste2/analysis.py
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------
+# UFRJ - Universidade Federal do Rio de Janeiro
+#   IM - Instituto de Matematica
+#  DMA - Departamento de Matematica Aplicada
+#
+# TMAA - MAE 353 - Programacao I
+#
+# Teste 2 - 18 de junho de 2019
+#
+# Aluna: Fabiana Ferreira
+# Aluna: Jackeline Carvalho
+# Aluna: Julia Brito
+#------------------
+
+import csv
+
+with open("./perfil_eleitorado_ATUAL.txt") as arq:
+    dados = csv.reader(arq, delimiter=';')
+    result = {}
+    for line in dados:
+        key = line[1]
+        if key not in result:
+            result[key] = int(line[8])
+        else:
+            result[key] += int(line[8])
+       
+    #Printing in a better way
+    for key, value in result.items():
+        print (key + ': ' + str(value))
+    soma = 0
+    for key in result:
+        soma = soma + int(result[key])
+    print ("Tamanho da amostra = " + str(soma))
